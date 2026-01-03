18aaac6d37ba6b192c204c88736fe7be5982f89e
Michael-A-Dev <126216824+Michael-A-Dev@users.noreply.github.com>
2026-01-03 14:35:21 +0800

    Day 2 (Part 2)

--- Days/Day2.cs
+++ Days/Day2.cs
@@ -57,6 +57,52 @@
 
         private void RunPart2(string[] ranges)
         {
+            List<long> invalids = [];
+
+            foreach (string range in ranges)
+            {
+                long startNum = Int64.Parse(range.Split('-')[0]);
+                long endNum = Int64.Parse(range.Split('-')[1]);
+
+                List<long> newInvalids = [];
+
+                for (long i = startNum; i <= endNum; i++)
+                {
+                    string iString = i.ToString();
+
+                    // Only checking for half number length and lower.
+                    for (int j = (iString.Length / 2); j > 0; j--)
+                    {
+                        if (iString.Length % j != 0) continue;
+
+                        IEnumerable<string> chunks = Enumerable.Range(0, iString.Length / j)
+                                                .Select(s => iString.Substring(s * j, j));
+
+                        int distinctChunks = chunks.Distinct().Count();
+
+                        if (distinctChunks == 1)
+                        {
+                            newInvalids.Add(i);
+                            break; // Stop doing any deeper checks, already invalid, move to next i
+                        }
+                    }
+
+                }
+
+                foreach (long num in newInvalids)
+                {
+                    invalids.Add(num);
+                }
+            }
+
+            long sumInvalids = 0;
+
+            foreach (long num in invalids)
+            {
+                sumInvalids += num;
+            }
+
+            Console.WriteLine("Day 2 (Part 2): " + sumInvalids);
         }
     }
 }
